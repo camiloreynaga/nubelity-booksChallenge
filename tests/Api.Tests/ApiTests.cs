6e630b595f6844e09ac8fb288f3e0c6495eba298
Camilo Reynaga <camilo.reynaga@gmail.com>
2025-12-29 03:27:50 -0500

    organization, folders for application, infra and test

---
+++ tests/Api.Tests/ApiTests.cs
@@ -0,0 +1,16 @@
+namespace Api.Tests;
+
+public class ApiTests
+{
+    [Fact]
+    public void Api_Project_Can_Be_Referenced()
+    {
+        // Arrange & Act
+        // This test validates that the API project can be referenced
+        // If the reference is correct, the project will compile
+        
+        // Assert
+        Assert.True(true);
+    }
+}
+
---
+++ tests/Application.Tests/ApplicationTests.cs
@@ -0,0 +1,16 @@
+namespace Application.Tests;
+
+public class ApplicationTests
+{
+    [Fact]
+    public void Application_Project_Can_Be_Referenced()
+    {
+        // Arrange & Act
+        // This test validates that the Application project can be referenced
+        // If the reference is correct, the project will compile
+        
+        // Assert
+        Assert.True(true);
+    }
+}
+
